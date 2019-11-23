6ac5c8c83751cb11d800148fe41d205761276ff4
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-23 14:44:29 +0000

    platina-user 8.1.0 OPM1.171019.019 9.1.24 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/xiaomi/platina
+BOARD_VENDOR := xiaomi
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/xiaomi/platina/BoardConfigVendor.mk
